0de096efd1ad083c3d92b116199c4407f14dd23e
Ranil <ranil2086@bk.ru>
2023-05-30 21:43:56 +0500

    server update / posgres

--- server/cors/passport.ts
+++ server/cors/passport.ts
@@ -1,6 +1,9 @@
 import passport from "passport";
 import { Strategy as GithubStrategy } from "passport-github";
 import dotenv from "dotenv";
+import { User } from "../../models";
+
+console.log(User);
 
 dotenv.config({ path: "./server/.env" });
 passport.use(
@@ -11,14 +14,43 @@ passport.use(
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackUrl: "http://localhost:3333/auth/github/callback",
     },
-    (accessToken, refreshToken, profile, cb) => {
-      const user = {
-        fullName: profile.displayName,
-        avatarUrl: profile.photos?.[0].value,
-      };
-      cb();
+    async (_: unknown, __: unknown, profile, done) => {
+      try {
+        const obj = {
+          fullName: profile.displayName,
+          avatarUrl: profile.photos?.[0].value,
+          isActive: 0,
+          username: profile.username,
+          phone: "",
+        };
+
+        const findUser = await User.findOne({
+          where: {
+            username: obj.username,
+          },
+        });
+
+        if (!findUser) {
+          const user = await User.create(obj);
+
+          return done(null, user.toJSON());
+        }
+
+        done(null, findUser);
+      } catch (error) {
+        done(error);
+      }
     }
   )
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    err ? done(err) : done(null, user);
+  });
+});
 export { passport };
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import session from "express-session";
 
 dotenv.config({
   path: "/server/.env",
@@ -11,13 +12,27 @@ import { passport } from "./cors/passport";
 const PORT = process.env.PORT;
 const app: Express = express();
 
+app.use(
+  session({
+    secret: "keyboard key",
+    resave: false,
+    saveUnitialized: true,
+    cookie: { secure: true },
+  })
+);
+app.use(passport.initialize());
+
 app.get("/auth/github", passport.authenticate("github"));
 
 app.get(
   "/auth/github/callback",
   passport.authenticate("github", { failureRedirect: "/login" }),
   (req: Request, res: Response) => {
-    res.send();
+    res.send(
+      `<script>window.opener.postMessage("${JSON.stringify(
+        req.user
+      )}", "*")</script>`
+    );
   }
 );
 
